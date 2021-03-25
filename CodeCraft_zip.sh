eb97f62377d8fe38a0637654449a6a5ccf5bde3d
Joen <kongy6@mail2.sysu.edu.cn>
2021-03-25 10:49:13 +0800

    文件压缩功能调整 1. 压缩文件名设置为 tagName-mm-dd-hh:mm.zip 2. 压缩文件的删除逻辑设置为同一个tagName只保留最新的一份

--- CodeCraft_zip.sh
+++ CodeCraft_zip.sh
@@ -4,12 +4,13 @@ SCRIPT=$(readlink "$0")
 BASEDIR=$(dirname "$SCRIPT")
 cd "$BASEDIR" || exit
 
-if [ ! -d CodeCraft-2021 ]
-then
-    echo "ERROR: $BASEDIR is not a valid directory of SDK_python for CodeCraft-2021."
-    echo "  Please run this script in a regular directory of SDK_python."
-    exit 255
+if [ ! -d CodeCraft-2021 ]; then
+  echo "ERROR: $BASEDIR is not a valid directory of SDK_python for CodeCraft-2021."
+  echo "  Please run this script in a regular directory of SDK_python."
+  exit 255
 fi
 
-rm -f CodeCraft-2021.zip
-zip -r CodeCraft-2021.zip ./CodeCraft-2021/*
+TAG_NAME=$(git describe --tags)
+
+rm -f "$TAG_NAME-"*.zip
+zip -r "$TAG_NAME-$(date +%m-%d-%H:%M).zip" -i="CodeCraft-2021/*/[a-zA-Z]*" -x="*/__pycache__/*" CodeCraft-2021
