2c10c8bbbd4396b8aa771bb9935c69566cce9215
Joen <kongy6@mail2.sysu.edu.cn>
2021-03-26 11:59:36 +0800

    增加对pypy3的支持

--- build_and_run.sh
+++ build_and_run.sh
@@ -6,12 +6,17 @@ cd "$BASEDIR" || exit
 
 cd CodeCraft-2021 || exit
 
+PY="python"
+if [ "$(whereis pypy3 | wc -l)" -ne 0 ]; then
+    PY="pypy3"
+fi
+
 if [ -n "$1" ]; then
     if [ -n "$2" ]; then
-      python src/CodeCraft-2021.py "$1" "$2"
+      $PY src/CodeCraft-2021.py "$1" "$2"
     else
-      python src/CodeCraft-2021.py "$1"
+      $PY src/CodeCraft-2021.py "$1"
     fi
 else
-    python src/CodeCraft-2021.py
+    $PY src/CodeCraft-2021.py
 fi
